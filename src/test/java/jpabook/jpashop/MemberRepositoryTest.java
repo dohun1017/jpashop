870b49d63c727c1815c94748e052ae47db208ad1
hoon <dohun10170@gmail.com>
2022-06-25 00:04:13 +0900

    프로젝트 환경설정 및 간단한 테스트

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional //test 코드 안에 있으면 롤백시켜버림
+    @Rollback(false) // @Rollback 어노테이션이 있으면 롤백시키지 않음.
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        assertThat(member.getId()).isEqualTo(findMember.getId());
+        assertThat(member.getUsername()).isEqualTo(findMember.getUsername());
+        assertThat(member).isEqualTo(findMember);
+    }
+
+}
