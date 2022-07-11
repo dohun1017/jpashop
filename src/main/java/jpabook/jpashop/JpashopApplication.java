607f8e0e6f1563970da2cdc47b76e1c95f7a22e3
hoon <dohun10170@gmail.com>
2022-07-11 20:55:42 +0900

    엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,8 +1,10 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @Slf4j
 @SpringBootApplication
@@ -11,4 +13,11 @@ public class JpashopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
+
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		return new Hibernate5Module();
+	}
+
+
 }
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -21,7 +21,7 @@ public class Member {
     @NotEmpty
     private String name;
 
-    /**
+    /* *
      * 보통은 둘다 표기해준다.
      * Address 클래스가 @Embeddable 어노테이션을 가지고 있어거나 사용하는 쪽에서 @Embedded 어노테이션을 사용해도 된다.
      */
